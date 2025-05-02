960ea221cd3c75d88777b519a2fa70ea4120ca1d
zaphrey <zachary.humphreys32@gmail.com>
2025-05-02 13:13:08 -0400

    Updated addProductToCart to push to an array instead of overwriting the local storage

--- src/js/product.js
+++ src/js/product.js
@@ -2,7 +2,18 @@ import { setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Parse the local storage cart into JSON
+  let cartStorage = JSON.parse(localStorage.getItem("so-cart"));
+  // If cart storage exists, push the new product to the array and set local storage again.
+  
+  if (cartStorage) {
+    cartStorage.push(product);
+    setLocalStorage("so-cart", cartStorage);
+  }
+  else {
+    // If cart storage doesn't exist, set the local storage to the product in an array.
+    setLocalStorage("so-cart", [ product ]);
+  }
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
